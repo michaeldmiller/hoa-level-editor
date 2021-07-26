8a4d28660b8e7c8567da982bbec3a031435d0f2c
Michael D. Miller <michaeldmiller3125@gmail.com>
2021-07-26 00:01:43 -0400

    v0.1.1, adds ability to add blocks

--- hoa-level-editor.py
+++ hoa-level-editor.py
@@ -1,12 +1,14 @@
-# Hero of Aeboria Level Editor, version 0.0.1
+# Hero of Aeboria Level Editor, version 0.1.1
 
-# changelog: imports Hero of Aeboria code and trims out unnecessary features
+# changelog: adds ability to scroll left and right and ability to add new
+# terrain blocks by pressing the mouse and "a" key
 
 
 # import modules
 import sys
 import pygame
 from pygame.locals import *
+import os
 vec = pygame.math.Vector2
 
 window_x_size = 1366
@@ -26,8 +28,9 @@ class Game:
         self.screen = pygame.display.set_mode((window_x_size, window_y_size))
         pygame.display.set_caption("Hero of Aeboria Level Editor")
         self.clock = pygame.time.Clock()
-        self.new_game()
         self.background = True
+        self.terrain_corners = []
+        self.new_game()
 
     def new_game(self):
         # sprite groups
@@ -41,6 +44,9 @@ class Game:
         self.background.fill(background_color)
 
         self.font = pygame.font.SysFont("cambria.ttf", 20)
+
+        # add text feedback holder
+        self.text_surface = True
 
         terrain_list = [(-200, 384, 250, 40),]    # starting ground
         for terrain_tuple in terrain_list:
@@ -62,6 +68,66 @@ class Game:
 
     def update(self):
         self.screen.blit(self.background, (0, 0))
+
+        # get key presses
+        keys = pygame.key.get_pressed()
+
+        # get mouse characteristics
+        mouse_presses = pygame.mouse.get_pressed()
+        mouse_down = False
+        if True in mouse_presses:
+            mouse_down = True
+        mouse_position = pygame.mouse.get_pos()
+
+        # move terrain left and right if those keys are pressed
+        if keys[K_LEFT]:
+            for all_terrain in self.terrain:
+                all_terrain.position.x -= 5
+        if keys[K_RIGHT]:
+            for all_terrain in self.terrain:
+                all_terrain.position.x += 5
+
+        # add to terrain corners list
+        if keys[K_a] and mouse_down:
+            if len(self.terrain_corners) < 2:
+                if mouse_position not in self.terrain_corners:
+                    self.terrain_corners.append(mouse_position)
+            # else:
+            #     if mouse_position not in self.terrain_corners:
+            #         del self.terrain_corners[0]
+            #         self.terrain_corners.append(mouse_position)
+
+        # clear terrain corners list
+        if keys[K_c]:
+            self.terrain_corners = []
+
+        # add new terrain block
+        if len(self.terrain_corners) == 2:
+            # find which mouse point is the left one
+            if self.terrain_corners[1][0] < self.terrain_corners[0][0]:
+                left_index, right_index = 1, 0
+            else:
+                left_index, right_index = 0, 1
+
+            # find which mouse point is the top one
+            if self.terrain_corners[1][1] < self.terrain_corners[0][1]:
+                top_index, bottom_index = 1, 0
+            else:
+                top_index, bottom_index = 0, 1
+
+            x_val = self.terrain_corners[left_index][0]
+            y_val = self.terrain_corners[bottom_index][1]
+            w_val = self.terrain_corners[right_index][0] - self.terrain_corners[left_index][0]
+            h_val = self.terrain_corners[bottom_index][1] - self.terrain_corners[top_index][1]
+
+            terrain_piece = TerrainElement(x_val, y_val, w_val, h_val)
+            self.all_sprites.add(terrain_piece)
+            self.terrain.add(terrain_piece)
+            self.terrain_corners = []
+
+        text_surface = self.font.render(str(self.terrain_corners), True, (0, 0, 0))
+        self.screen.blit(text_surface, (40, 50))
+
         self.all_sprites.update()
         self.all_sprites.draw(self.screen)
         pygame.display.flip()
@@ -72,6 +138,9 @@ class Game:
                 sys.exit()
             # elif event.type == MOUSEBUTTONDOWN:
             #     self.first_sound.play()
+
+
+
 
 
 class TerrainElement(pygame.sprite.Sprite):
